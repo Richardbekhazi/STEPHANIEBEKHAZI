4fd44c3d07ea8f2fca7db343e9dcc7b3e618d5ca
Richard <RichardBekhazi@gmail.com>
2025-11-25 12:53:33 -0500

    Build site to docs for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',          // custom domain = root, so this is fine
+  build: {
+    outDir: 'docs',   // <— IMPORTANT: GitHub Pages will serve from /docs
+  },
 })
